17b80818bfc7a2e7eb7d41b0cb3feae3fba6b01c
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-06-27 10:14:02 +0000

    make it my web app logo

--- src/components/rdc-logo.tsx
+++ src/components/rdc-logo.tsx
@@ -3,16 +3,23 @@ import React from 'react';
 export function RdcLogo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 130 50"
+      viewBox="0 0 120 50"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
       aria-label="Red Dot Classroom Logo"
       {...props}
     >
-      <path d="M0 50 V25 C0 11.19 11.19 0 25 0 H35 V12 H25 C17.82 12 12 17.82 12 25 V50 H0Z" fill="#177DFF"/>
-      <circle cx="58" cy="25" r="25" fill="#FFC107"/>
-      <rect x="71" y="0" width="12" height="50" fill="#177DFF"/>
-      <path d="M118 0 C 103 0 93 10 93 25 C 93 40 103 50 118 50 H130 C 115 50 105 40 105 25 C 105 10 115 0 130 0 H118 Z" fill="#F93232"/>
+      <g>
+        {/* r */}
+        <path d="M0 50V0H35C35 13.8 24.7 25 12 25V50H0Z" fill="#177DFF"/>
+        
+        {/* d */}
+        <circle cx="65" cy="25" r="25" fill="#FFC107"/>
+        <rect x="78" y="0" width="12" height="50" fill="#177DFF"/>
+        
+        {/* c */}
+        <path d="M120 0C106.193 0 95 11.1929 95 25C95 38.8071 106.193 50 120 50V0Z" fill="#F93232"/>
+      </g>
     </svg>
   );
 }
