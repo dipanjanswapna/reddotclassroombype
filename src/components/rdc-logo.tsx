f7b9cfde4b9d5a8342c0137113346e100b6189ab
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-06-27 12:05:38 +0000

    RED DOT CLASSROOM (RDC): একটি পূর্ণাঙ্গ ওয়েব অ্যাপ্লিকেশন - বিস্তারিত ব

--- src/components/rdc-logo.tsx
+++ src/components/rdc-logo.tsx
@@ -11,14 +11,14 @@ export function RdcLogo({ className, ...props }: React.SVGProps<SVGSVGElement>) 
     >
       <g>
         {/* r */}
-        <path d="M0 50V0H35C35 13.8 24.7 25 12 25V50H0Z" fill="#177DFF"/>
+        <path d="M0 50V0H35C35 13.8 24.7 25 12 25V50H0Z" fill="#333333"/>
         
         {/* d */}
-        <circle cx="65" cy="25" r="25" fill="#FFC107"/>
-        <rect x="78" y="0" width="12" height="50" fill="#177DFF"/>
+        <circle cx="65" cy="25" r="25" fill="#333333"/>
+        <rect x="78" y="0" width="12" height="50" fill="#333333"/>
         
         {/* c */}
-        <path d="M120 0C106.193 0 95 11.1929 95 25C95 38.8071 106.193 50 120 50V0Z" fill="#F93232"/>
+        <path d="M120 0C106.193 0 95 11.1929 95 25C95 38.8071 106.193 50 120 50V0Z" fill="#8B1538"/>
       </g>
     </svg>
   );
