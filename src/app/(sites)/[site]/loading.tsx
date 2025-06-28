253539b3abb2a1b79ac6b6f414b2c074a5142cd2
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-06-28 10:53:10 +0000

    go to next step for fulfill my web app front end and backend set up all

--- src/app/(sites)/[site]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from '@/components/loading-spinner';
-
-export default function Loading() {
-  return (
-    <div className="flex flex-grow items-center justify-center h-full w-full p-8">
-      <LoadingSpinner className="w-12 h-12" />
-    </div>
-  );
-}
--- src/backend/ai/dev.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/backend/ai/flows/virtual-tutor-chatbot.ts';
