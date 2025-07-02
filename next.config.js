b09b2725933d86a9d5197a32f2f1f5b50aca8126
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-02 07:24:12 +0000

    id card e qr code er system bad diye auto genarated bar code er system g

---
+++ next.config.js
@@ -0,0 +1,38 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {},
+  allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
