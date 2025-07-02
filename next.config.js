cbb5630aa6a8ff784b2da66c8509f0a2edabb793
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-02 07:27:29 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -26,12 +27,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.qrserver.com',
         port: '',
         pathname: '/**',
       },
