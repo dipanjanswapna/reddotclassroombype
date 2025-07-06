5eaf3f85f5894dc436cd4bd5d184280241462fc3
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-06 09:01:09 +0000

    admin panel e system ti chalu hoy ni please gora theke system ti chalu k

--- src/app/admin/manage-user/[userId]/page.tsx
+++ src/app/admin/manage-user/[userId]/page.tsx
@@ -136,7 +136,7 @@ export default function ManageUserPage() {
     }, [attendance, allCourses]);
 
     if (loading) {
-        return <LoadingSpinner className="w-12 h-12 m-auto" />;
+        return <div className="flex items-center justify-center h-[calc(100vh-8rem)]"><LoadingSpinner className="w-12 h-12 m-auto" /></div>;
     }
 
     if (!user) {
