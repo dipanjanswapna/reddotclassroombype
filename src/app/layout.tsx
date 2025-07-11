621d2c8e79b278a1293e3326eb385c5f42b0fc8d
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-11 12:51:10 +0000

    <!--Start of Tawk.to Script--> <script type="text/javascript"> var Tawk_

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,12 +60,11 @@ export default async function RootLayout({
         </AuthProvider>
         <Script id="tawk-to-script" strategy="lazyOnload">
           {`
-            var Tawk_API=Tawk_API||{};
-            var Tawk_LoadStart=new Date();
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/66a4f213e160454749320e40/1i3qj4gct';
+            s1.src='https://embed.tawk.to/68700613affcbd1910f86331/1ivqpffc4';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
