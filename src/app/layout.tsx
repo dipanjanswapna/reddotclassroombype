3b92de2ea80146bc29162c6d09a2676fb3c16182
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-11 12:47:08 +0000

    same somossa dekha jacce etar gora theke somadhan koro.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,15 +60,15 @@ export default async function RootLayout({
         </AuthProvider>
         <Script id="tawk-to-script" strategy="lazyOnload">
           {`
-            var Tawk_API = Tawk_API || {};
-            var Tawk_LoadStart = new Date();
-            (function() {
-              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-              s1.async = true;
-              s1.src = 'https://embed.tawk.to/66a4f213e160454749320e40/1i3qj4gct';
-              s1.charset = 'UTF-8';
-              s1.setAttribute('crossorigin', '*');
-              s0.parentNode.insertBefore(s1, s0);
+            var Tawk_API=Tawk_API||{};
+            var Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66a4f213e160454749320e40/1i3qj4gct';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
             })();
           `}
         </Script>
