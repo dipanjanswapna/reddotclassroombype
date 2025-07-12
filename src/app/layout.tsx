762c2bf7d07eaecad9b44a9e4b15bf5bc9cff390
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-12 12:27:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,23 +56,6 @@ export default async function RootLayout({
         <LayoutWrapper homepageConfig={homepageConfig}>
             {children}
         </LayoutWrapper>
-        <Script id="tawk-to-script" strategy="lazyOnload">
-          {`
-            var Tawk_API=Tawk_API||{};
-            Tawk_API.onLoad = function(){
-                Tawk_API.hideWidget();
-            };
-            var Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/68700613affcbd1910f86331/1ivqpffc4';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          `}
-        </Script>
       </body>
     </html>
   );
