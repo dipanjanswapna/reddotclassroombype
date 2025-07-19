82cb30010a16d8ba8e0a9e640d4fff4e04bc3583
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-19 19:29:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,9 +1,14 @@
 
-import { auth } from './config';
+import { getAuthInstance } from './config';
 // A server-side helper to get the current user.
 // NOTE: This will only work in server components that are part of a dynamic route.
 // For static routes, the user will always be null during build time.
 // Using this in a client component will result in an error.
 export const getCurrentUser = async () => {
+    const auth = getAuthInstance();
+    if (!auth) {
+        // This might happen during build time or if Firebase is not configured.
+        return null;
+    }
     return auth.currentUser;
 }
