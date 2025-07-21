2285104f766ca71367386570b9d3a63236ffb89a
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2025-07-21 07:19:16 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,21 +1,3 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of your edge features is run.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-  _experiments: {
-    enableLogs: true,
-  },
-   integrations: [
-
-  ],
-});
+// This file has been deprecated in favor of src/instrumentation.ts
+// The Sentry SDK will automatically pick up the new file.
+// This file can be safely deleted.
